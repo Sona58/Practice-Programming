4ffef4abe7704730a6fc9aaad8d87f030f66fa5c
Sonali Chawla <s2sonali@gmail.com>
2016-09-03 18:40:24 +0530

    Add files via upload

---
+++ JAVA/pk/pack.java
@@ -0,0 +1,9 @@
+package pk;
+
+public class pack
+{
+	public void display()
+	{
+		System.out.println("Pack class in Package pk.");
+	}
+};
---
+++ JAVA/pk/pack1.java
@@ -0,0 +1,9 @@
+package pk;
+
+public class pack1
+{
+	public void display1()
+	{
+		System.out.println("Pack1 class in package pk.");
+	}
+};
