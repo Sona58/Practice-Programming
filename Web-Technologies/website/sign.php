bbedd5392271c809da9fc31d3e7fea273f255712
Sonali Chawla <s2sonali@gmail.com>
2016-09-03 19:21:51 +0530

    Add files via upload

---
+++ Web-Technologies/website/sign.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Book My Show</title>
+</head>
+<body onresize=resizeTo(250,250)>
+<form>
+Enter Email:<br>
+<input type="text" name="email"/><br><br>
+Enter Password:<br>
+<input type="password" name="pass"/><br><br>
+<a href="index.html"><input type="button" name="submit" value="Sign In"/></a>
+
+
+</form>
+</body>
+</html>
---
+++ Web-Technologies/website/validate.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Validation</title>
+</head>
+<body>
+<u><h2>Details Filled:</h2></u>
+<?php
+echo "<b>Name:</b> ".$_POST['name'];
+echo "<br><br><b>Login Id:</b> ".$_POST['loginid'];
+echo "<br><br><b>Password:</b> ".$_POST['pwd'];
+echo "<br><br><b>Gender:</b> ".$_POST['r1'];
+echo "<br><br><b>Qualification:</b> ".$_POST['qual'];
+echo "<br><br><b>Address:</b> ".$_POST['add'];
+?>
+</body>
+</html>
