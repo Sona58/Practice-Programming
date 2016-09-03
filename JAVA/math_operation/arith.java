757b6c560f02de80373d49149faec4c940e45508
Sonali Chawla <s2sonali@gmail.com>
2016-09-03 18:39:08 +0530

    Add files via upload

---
+++ JAVA/math_operation/arith.java
@@ -0,0 +1,24 @@
+package math_operation;
+
+public class arith
+{
+	public void add(int a,int b)
+	{
+		System.out.println("Sum: "+(a+b));
+	}
+	
+	public void sub(int a,int b)
+	{
+		System.out.println("Difference: "+(a-b));
+	}
+	
+	public void div(int a,int b)
+	{
+		System.out.println("Quotient: "+(a/b));
+	}
+
+	public void mul(int a,int b)
+	{
+		System.out.println("Product: "+(a*b));
+	}
+};
